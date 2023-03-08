7baeae103dff64eb528ce454d82dd4405eb268b4
PICONNFQ <126184754+PICONNFQ@users.noreply.github.com>
2023-03-08 12:56:26 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,6 @@ try:
       back_from_function = get_fruityvice_data(fruit_choice)
       streamlit.dataframe(back_from_function)
 
-    
 except URLError as e:
   streamlit.error()
 
@@ -52,13 +51,11 @@ streamlit.stop()
 
 # import snowflake.connector
 
-
-
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 # Snowflake-related functions
 def get_fruit_load_list():
-  with my_cnx.cursor as my_cur:
+  with my_cnx.cursor() as my_cur:
     my_cur.execute("select * from fruit_load_list")
     return my_cyr.fetchall()
   
